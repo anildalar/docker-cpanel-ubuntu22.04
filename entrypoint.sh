82d439b62b6d9538a7da53cbbf53eaeb5cfeb674
anildalar <dollorinfotech@gmail.com>
2024-10-24 07:49:31 +0200

    new

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,19 @@ else
   echo "No root password provided. Skipping password update."
 fi
 
+# Check if UPDATE_CPANEL_TO_LATEST is set to true
+if [ "$UPDATE_CPANEL_TO_LATEST" == "true" ]; then
+  echo "Updating cPanel to the latest version..."
+  /scripts/upcp --force
+  if [ $? -eq 0 ]; then
+    echo "cPanel updated successfully."
+  else
+    echo "cPanel update failed."
+    exit 1
+  fi
+else
+  echo "Skipping cPanel update."
+fi
+
 # Start systemd to ensure all services, including cPanel, are managed correctly
 exec /lib/systemd/systemd
